e22659bd4b9f070f3e8c9c8622adece9711d3b58
YdeaGames <ydeagames@gmail.com>
2019-04-18 18:36:06 +0900

    Create and Export

---
+++ Assets/StageEditor/Editor/CreateStage.cs
@@ -0,0 +1,36 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class CreateStage : ScriptableWizard
+{
+    [Header("<名前>.<s番号>")]
+    public string stageName = "<名前>.<s番号>";
+
+    [Header("ステージ名")]
+    public string stageTitle = "無名のステージ";
+
+    [MenuItem("ステージ作成/ステージを新規作成")]
+    static void CreateWizard()
+    {
+        ScriptableWizard.DisplayWizard<CreateStage>("ステージ作成", "作成");
+    }
+
+    void OnWizardCreate()
+    {
+        var dir = AssetDatabase.GUIDToAssetPath(AssetDatabase.CreateFolder("Assets/Stages", stageName));
+        var scene = dir + "/" + stageName + ".unity";
+        var stage = dir + "/" + stageName + ".asset";
+        AssetDatabase.CopyAsset(@"Assets/StageEditor/Template/Template.unity", scene);
+        AssetDatabase.CopyAsset(@"Assets/StageEditor/Template/Template.asset", stage);
+        Stage stageobj = AssetDatabase.LoadAssetAtPath(stage, typeof(ScriptableObject)) as Stage;
+        stageobj.sceneName = stageName;
+        stageobj.stageName = stageTitle;
+        EditorUtility.SetDirty(stageobj);
+        AssetDatabase.Refresh();
+    }
+
+    void OnWizardUpdate()
+    {
+        helpString = "ステージ名を入れて作成ボタンを押してください。";
+    }
+}
---
+++ Assets/StageEditor/Editor/ExportStage.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ExportStage : ScriptableWizard
+{
+    [Header("<名前>.<s番号>")]
+    public string stageName = "<名前>.<s番号>";
+
+    [MenuItem("ステージ作成/ステージをエクスポート")]
+    static void CreateWizard()
+    {
+        ScriptableWizard.DisplayWizard<ExportStage>("ステージをエクスポート", "エクスポート");
+    }
+
+    void OnWizardCreate()
+    {
+        var guids = AssetDatabase.FindAssets("", new string[]{
+            "Assets/Stages/" + stageName
+        });
+
+        var assets = new string[guids.Length];
+        for (int i = 0; i < guids.Length; ++i)
+            assets[i] = AssetDatabase.GUIDToAssetPath(guids[i]);
+
+        var file = EditorUtility.SaveFilePanel("ステージをエクスポート", "Assets/../..", stageName, "unitypackage");
+        if (!string.IsNullOrEmpty(file))
+            AssetDatabase.ExportPackage(assets, file, ExportPackageOptions.Recurse);
+    }
+
+    void OnWizardUpdate()
+    {
+        helpString = "エクスポートしたいステージ名を入れてエクスポートボタンを押してください。";
+    }
+}
