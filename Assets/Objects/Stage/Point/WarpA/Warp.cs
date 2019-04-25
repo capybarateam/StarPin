bc26ced11a8f658e2fb445e668cace2a6b47ea4c
liknlie7 <liknlie@gmail.com>
2019-04-25 17:25:47 +0900

    #

--- Assets/Objects/Stage/Point/WarpA/Warp.cs
+++ Assets/Objects/Stage/Point/WarpA/Warp.cs
@@ -1,17 +1,29 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Warp : MonoBehaviour
+public class Warp : MonoBehaviour, IAttachable
 {
 
-    [SerializeField]
-    GameObject warp_in;
+    //[SerializeField]
+    //GameObject warp_in;
 
     [SerializeField]
     GameObject warp_out;
 
-    
+    // 引っ付けるかどうか
+    public void CheckAttachable(StarController star, ref bool cancel)
+    {
+    }
+
+    // 引っ付いたときの処理
+    public void OnAttached(StarController star)
+    {
+        star.currentJoint = warp_out;
+    }
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -23,11 +35,6 @@ public class Warp : MonoBehaviour
     void Update()
     {
         
-    }
-
-    void OnTriggerEnter2D(Collider2D collision)
-    {
-
     }
 
 
