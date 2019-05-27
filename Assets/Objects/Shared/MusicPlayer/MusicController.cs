9ce91866bfa07948bd49ea0aa9ad7a29f4a34fdb
YdeaGames <ydeagames@gmail.com>
2019-05-27 17:42:40 +0900

    FMOD2

---
+++ Assets/Objects/Shared/MusicPlayer/MusicController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+[RequireComponent(typeof(StudioEventEmitter))]
+public class MusicController : MonoBehaviour
+{
+    StudioEventEmitter eventEmitter;
+    private void Awake()
+    {
+        eventEmitter = GetComponent<StudioEventEmitter>();
+    }
+
+    public void ApplyParamater()
+    {
+        var instance = eventEmitter.EventInstance;
+        foreach (var p in eventEmitter.Params)
+        {
+            //instance.setParameterValue(p.Name, p.Value);
+        }
+    }
+
+    public void ChangeParameter(string paramName, float value)
+    {
+        //eventEmitter.SetParameter(paramName, value);
+    }
+}
